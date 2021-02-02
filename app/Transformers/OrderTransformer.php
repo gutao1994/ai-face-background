5ac383f5b0140de04bedb37a92903cc60f60db52
unknown <gutao@test.com>
2021-02-02 23:30:40 +0800

    full img url

--- app/Transformers/OrderTransformer.php
+++ app/Transformers/OrderTransformer.php
@@ -10,7 +10,17 @@ class OrderTransformer extends TransformerAbstract
 
     public function transform(Order $order)
     {
-        return $order->attributesToArray();
+        $attr = $order->attributesToArray();
+
+        if (!empty($attr['img']))
+            $attr['img'] = $this->getUrl($attr['img']);
+
+        return $attr;
+    }
+
+    protected function getUrl($path)
+    {
+        return 'https://' . config('filesystems.disks.oss.cdnDomain') . '/' . ltrim($path, '/');
     }
 
 
