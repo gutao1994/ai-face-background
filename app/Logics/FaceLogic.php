72014e6fab558abb22e5881512fcb410dc7020d9
GUTAO <GUTAO@163.com>
2021-02-18 17:40:50 +0800

    未完待续

--- app/Logics/FaceLogic.php
+++ app/Logics/FaceLogic.php
@@ -33,9 +33,13 @@ class FaceLogic
 
     protected $faceType;
 
+    protected $jawType;
+
     protected $eyebrowType;
 
     protected $eyesType;
+
+    protected $noseType;
 
     protected $mouthType;
 
@@ -77,8 +81,10 @@ class FaceLogic
         $this->lefteyeEmptyResult = $face['result']['five_eyes']['five_eye']['lefteye_empty_result']; //五眼左侧距判断结果
         $this->goldenTriangle = $face['result']['golden_triangle']; //黄金三角度数
         $this->faceType = $face['result']['face']['face_type']; //脸型判断结果
+        $this->jawType = $face['result']['jaw']['jaw_type']; //下巴判断结果
         $this->eyebrowType = $face['result']['eyebrow']['eyebrow_type']; //眉型判断结果
         $this->eyesType = $face['result']['eyes']['eyes_type']; //眼型判断结果
+        $this->noseType = $face['result']['nose']['nose_type']; //鼻翼判断结果
         $this->mouthType = $face['result']['mouth']['mouth_type']; //唇型判断结果
         $this->eyePouch = $skin['result']['eye_pouch']['value']; //眼袋检测结果
         $this->darkCircle = $skin['result']['dark_circle']['value']; //黑眼圈检测结果
@@ -89,15 +95,15 @@ class FaceLogic
         $this->nasolabialFold = $skin['result']['nasolabial_fold']['value']; //法令纹检测结果
         $this->acne = $skin['result']['acne']['value']; //痘痘检测结果
 
-        if ($this->age <= 10) {
+        if ($this->age <= 10) { //0-10
             return $this->sex == 'Male' ? $this->male10() : $this->female10();
-        } elseif ($this->age <= 19) {
+        } elseif ($this->age <= 19) { //11-19
             return $this->sex == 'Male' ? $this->male19() : $this->female19();
-        } elseif ($this->age <= 26) {
+        } elseif ($this->age <= 26) { //20-26
             return $this->sex == 'Male' ? $this->male26() : $this->female26();
-        }elseif ($this->age <= 35) {
+        }elseif ($this->age <= 35) { //27-35
             return $this->sex == 'Male' ? $this->male35() : $this->female35();
-        } elseif ($this->age <= 50) {
+        } elseif ($this->age <= 50) { //35-50
             return $this->sex == 'Male' ? $this->male50() : $this->female50();
         } else { //>50
             return $this->sex == 'Male' ? $this->maleInf() : $this->femaleInf();
@@ -115,6 +121,11 @@ class FaceLogic
             } else {
                 $this->result .= '有些时候孩子的意志力会比较不坚定，做事容易优柔寡断，在处理与人交往的事情上会有所欠缺，但却是一个为人正直、能够快速判断是非的人。所以家长平时应该注重提升孩子的意志力，让孩子在将来的人生中不为一些不必要的事情所困扰。';
             }
+        } elseif ($this->mouthType == 'smile_lip' || $this->emotion == 'happiness') { //微笑唇 或者 高兴的情绪
+            $this->result = '你是一个性格开朗、乐观、自信、不爱生气的人，你所在的家庭也是一个氛围良好和睦的家庭。你往往是大家的开心果，身边的人都会很喜欢你。你长大之后，对生活、工作和感情都能够长时间保持激情、热情，总是以积极乐观的心态去面对生活中的人或事。';
+            $this->result .= '烦恼、忧愁对你的影响都不大，因为你总是能够自主地调节这类不良的情绪，让积极乐观占据主动，不让这些不良的情绪影响生活和工作，所以你的一生都会生活地很轻松、幸福、快乐。';
+        } elseif ($this->eyeinResult == 'eyein_long') { //内眼角间距偏宽
+
         } else {
 
         }
