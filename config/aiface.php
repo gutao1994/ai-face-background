411d213c95388b891c5006ddcfbd50e8aeec67bb
GUTAO <GUTAO@163.com>
2021-04-12 17:08:44 +0800

    未完待续

--- config/aiface.php
+++ config/aiface.php
@@ -10,7 +10,17 @@ return [
     /**
      * 分享佣金返利金额
      */
-    'share_commission_price' => 0.2,
+    'share_commission_price' => [
+        'default' => 1,
+
+        'level' => [
+            1 => 0.2,
+            2 => 0.4,
+            3 => 0.6,
+            4 => 0.8,
+            5 => 1.0,
+        ],
+    ],
 
     /**
      * 允许api调用失败的最大次数
